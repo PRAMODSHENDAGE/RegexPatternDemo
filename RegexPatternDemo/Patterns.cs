21594bbbc9e43c165a062f9ac54279f1245fc798
PRAMODSHENDAGE <shendagepramod@gmail.com>
2022-05-10 08:44:50 +0530

    Add project files.

---
+++ RegexPatternDemo/Patterns.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace RegexPatternDemo
+{
+    internal class Patterns
+    {
+        public static string REGEX_PINCODE = "^[1-9][0-9]{5}$";
+        public static string REGEX_PHONENUMBER = @"^(((\+){1}91){1}) ?-?[6-9]{1}[0-9]{9}$";
+        public static string REGEX_EMAIL = "^[0-9a-zA-Z]+[.]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
+        public string REGEX_PASSWORD = "^[A-Z]{1}[a-z]{2}[!@#$]{1}[0-9]{4}$";
+        public bool validatePinCode(string pincode) 
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+        public bool validatePhoneNumber(string phonenumber)
+        {
+            return Regex.IsMatch(phonenumber, REGEX_PHONENUMBER);
+        }
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
---
+++ RegexPatternDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RegexPatternDemo;
+Patterns patterns = new Patterns();
+Console.WriteLine(patterns.validatePinCode("410218"));
+
+Patterns patterns1 = new Patterns();
+Console.WriteLine(patterns1.validatePhoneNumber("+91-9898989898"));
+
+Patterns patterns2 = new Patterns();
+Console.WriteLine(patterns2.validateEmail("adh212au.parag64@gmail.co.in"));
+
+Patterns patterns3 = new Patterns();
+Console.WriteLine(patterns3.validatePassword("Pqr@2123"));
